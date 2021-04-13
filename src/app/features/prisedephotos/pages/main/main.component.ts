b356110e34628b01f3b0a96c11baf03886e918fa
RichardDevaux <74962446+RichardDevaux@users.noreply.github.com>
2021-04-13 10:59:48 +0200

    20210413 11:00

--- src/app/features/prisedephotos/pages/main/main.component.ts
+++ src/app/features/prisedephotos/pages/main/main.component.ts
@@ -3,6 +3,7 @@ import { FichiertemporaireService } from '../../services/fichiertemporaire.servi
 import { UnePhotoTemporaire } from '../../services/requests.model';
 import { v4 as uuidv4 } from 'uuid';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { stringify } from '@angular/compiler/src/util';
 
 
 @Component({
@@ -42,47 +43,27 @@ export class MainComponent implements OnInit {
         uuid: uuidv4()
       };
 
-      /*
-      this.file = files.item(0);
-      const photoselection = event.target.files;
-      const reader = new FileReader();
-      reader.addEventListener('load', () => {
-        const SmallImg = new Image();
-        const bigImg = new Image();
-        let maphotoBase64 = '';
-        let mavignetteBase64 = '';
-
-        bigImg.src = reader.result;
-        bigImg.onload = function (el) {
-          maphotoBase64 = imageToDataUrl(bigImg, 4000, 3000);
-          SmallImg.src = reader.result;
-          SmallImg.onload = function (el) {
-            mavignetteBase64 = imageToDataUrl(SmallImg, 800, 600);
-            yvidia.envoiPhoto(event.target.files[0].name, maphotoBase64, mavignetteBase64);
-          };
-        };
-      }, false);
-      reader.readAsDataURL(photoselection[0]);
-      */
-
       const reader = new FileReader();
 
       reader.onload = (e: any) => {
 
         const imageSmall = new Image();
         const imageBig = new Image();
-        const maphotoBase64 = '';
+        let maphotoBase64 = '';
         let mavignetteBase64 = '';
 
         imageSmall.src = e.target.result;
         imageSmall.onload = rs => {
 
-          /*maphotoBase64 = imageToDataUrl(image, 4000, 3000);*/
-
           imageBig.src = e.target.result;
           imageBig.onload = rt => {
 
+            maphotoBase64 = this.imageToDataUrl(imageBig, 4000, 3000);
             mavignetteBase64 = this.imageToDataUrl(imageSmall, 800, 600);
+
+            monJson.contenu = maphotoBase64;
+            monJson.apercu = mavignetteBase64;
+
             // tslint:disable-next-line: deprecation
             this.yvidia.postFichierTemporaire(monJson).subscribe(
               {
@@ -117,53 +98,62 @@ export class MainComponent implements OnInit {
 
   imageToDataUrl(img: HTMLImageElement, widthImg: number, heightImg: number): string {
 
-    return '';
-  }
+    let imageRetour = '';
 
-  /*
- imageToDataUrl(img: string, _width: number, _height: number): string {
+    const maxWidth = widthImg;
+    const maxHeight = heightImg;
+    let width = img.width;
+    let height = img.height;
 
-  let canvas = document.createElement('canvas'),
-    imageResult = document.createElement('img'),
-    ctx,
-    maxWidth = _width,
-    maxHeight = _height,
-    width = img.width,
-    height = img.height;
-
-  if (width > height) {
-    if (width > maxWidth) {
-      height *= maxWidth / width;
-      width = maxWidth;
+    if (width > height) {
+      if (width > maxWidth) {
+        height *= maxWidth / width;
+        width = maxWidth;
+      }
+    } else {
+      if (height > maxHeight) {
+        width *= maxHeight / height;
+        height = maxHeight;
+      }
     }
-  } else {
-    if (height > maxHeight) {
-      width *= maxHeight / height;
-      height = maxHeight;
+
+    if (width !== img.width || height !== img.height) {
+      imageRetour = this.compressImage(img.src, width, height);
+    } else {
+      imageRetour = img.src;
     }
+
+    imageRetour = imageRetour.split(',')[1];
+
+    return imageRetour;
+
   }
-
-  canvas.width = width;
-  canvas.height = height;
-  ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0, width, height);
-
-  imageResult.addEventListener('load', () => {
-
-    const retour = '';
-
-  });
-
-  imageResult = canvas.toDataURL('image/jpeg');
-  imageResult = imageResult.split(',')[1];
-
-  return imageResult;
-
-}
-*/
 
   base64ToDataUri(base64: any): string {
     return `data:image/jpg;base64,${base64}`;
+  }
+
+  compressImage(src: string, newX: number, newY: number): string {
+
+    let imgCompressed = '';
+
+    const img = new Image();
+    img.src = src;
+    const elem = document.createElement('canvas');
+    elem.width = newX;
+    elem.height = newY;
+    const ctx = elem.getContext('2d');
+    if (ctx != null) {
+      ctx.drawImage(img, 0, 0, newX, newY);
+      elem.addEventListener('load', () => {
+
+      });
+      imgCompressed = elem.toDataURL('image/jpeg');
+
+
+    }
+
+    return imgCompressed;
   }
 
 }
